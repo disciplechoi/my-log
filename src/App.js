3efbf257764117457c3ffbbfcca9cdb4211d22b2
Jihye Choi <jihyechoi@Mac-mini.usp.local>
2022-06-17 14:51:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import LightGallery from 'lightgallery/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+// // If you want you can use SCSS instead of css
+// import 'lightgallery/scss/lightgallery.scss';
+// import 'lightgallery/scss/lg-zoom.scss';
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+function Gallery() {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+    return (
+        <div className="App">
+            <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom]}
+            >
+                <a href="img/img1.jpg">
+                    <img alt="img1" src="img/thumb1.jpg" />
+                </a>
+                <a href="img/img2.jpg">
+                    <img alt="img2" src="img/thumb2.jpg" />
+                </a>
+                ...
+            </LightGallery>
+        </div>
+    );
 }
 
-export default App;
+
+export default Gallery;
